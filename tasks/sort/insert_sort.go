721153e06a24c13cd2c60a1ed1cf09b5c82f7ccf
Ayaz Ayupov <ayaz@releaseband.com>
2022-12-25 19:23:36 +0700

    added insert sort

---
+++ tasks/sort/insert_sort.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func InsertSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		itemToInsert := arr[i]
+
+		j := i
+		for ; j > 0 && itemToInsert < arr[j-1]; j-- {
+			arr[j] = arr[j-1]
+		}
+
+		arr[j] = itemToInsert
+		fmt.Printf("step %d sorted %d elements: %v\n", i, i+1, arr)
+	}
+}
+
+func main() {
+	arr := []int{10, 9, 7, 5, 4, 2, 1}
+	InsertSort(arr)
+}
